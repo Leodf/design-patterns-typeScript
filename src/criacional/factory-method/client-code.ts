f78c4b8fad8c0e85c14cfcc20343c42c23ca1f07
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-29 16:27:47 -0300

    aplicando o factory method

---
+++ src/criacional/factory-method/client-code.ts
@@ -0,0 +1,16 @@
+import { CarFactory } from "./factories/car-factory";
+import { randomCar } from "./main/random-vehicle";
+import { randomNumbers } from "./utils/randomNumbers";
+
+const carFactory = new CarFactory()
+const customerNames = ['Ana', 'Joana', 'Helena', 'João']
+
+for (let i = 0; i < 10; i++) {
+    const vehicle = randomCar()
+    const name = customerNames[randomNumbers(customerNames.length)]
+    vehicle.pickUp(name)
+    vehicle.stop()
+    const newCar = carFactory.pickUp(name, `Novo Carro - ${randomNumbers(100)}`)
+    newCar.stop()
+    console.log('---')
+}
---
+++ src/criacional/factory-method/main/random-vehicle.ts
@@ -0,0 +1,15 @@
+import { BicycleFactory } from "../factories/bicycle-factory";
+import { CarFactory } from "../factories/car-factory";
+import { randomNumbers } from "../utils/randomNumbers";
+import { Vehicle } from "../vehicle/vehicle";
+
+export function randomCar(): Vehicle {
+    const carFactory = new CarFactory()
+    const bicycleFactory = new BicycleFactory()
+
+    const car1 = carFactory.getVehicle('Fusca')   
+    const car2 = carFactory.getVehicle('Celta preto')
+    const bike = bicycleFactory.getVehicle('Bicicleta')
+    const cars = [car1, car2, bike]
+    return cars[randomNumbers(cars.length)]
+}
