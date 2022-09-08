a851293c609873923fd508c07ea710d4835934ad
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-08 09:28:25 -0300

    commit

--- src/fintech/core/produto/Produto.ts
+++ src/fintech/core/produto/Produto.ts
@@ -2,7 +2,4 @@
 import { Receita } from "./Receita"
 import { Despesa } from "./Despesa"
 
-export abstract class Produto {
-    receitas: Receita[] = []
-    despesas: Despesa[] = []
-}
+export abstract class Produto {}
--- src/fintech/main.ts
+++ src/fintech/main.ts
@@ -3,10 +3,18 @@ import { Perfil } from './core/Perfil'
 import { ReceitaFabrica } from "./core/produtoFabrica/ReceitaFabrica"
 import { DespesaFabrica } from "./core/produtoFabrica/DespesaFabrica"
 
-const Maria = new Perfil('Maria', 'maria@gmail.com.br', '654321', '19/08/2002', 'feminino')
 const Joao = new Perfil('João', 'joao@gmail.com.br', '123456', '17/08/2001', 'masculino')
+
 const usuario1 = new Usuario(1, Joao)
-const usuario2 = new Usuario(2, Maria)
+
+usuario1.adicionarCarteira('vaiTourinho', '05/09/2022')
+usuario1.adicionarCarteira('Itau', '10/09/2022')
+usuario1.adicionarCarteira('Santander', '15/09/2022')
+
+// console.log(usuario1)
+usuario1.alterarNomeCarteira(0, 'vaiUrsinhoooo')
+// console.log(usuario1)
+
 
 const receitaFabrica = new ReceitaFabrica()
 const receita1 = receitaFabrica.adicionarProduto('05/09/2022', 'Aluguel', 1200)
@@ -15,14 +23,12 @@ const receita2 = receitaFabrica.adicionarProduto('05/09/2022', 'Salario', 300)
 const despesaFabrica = new DespesaFabrica()
 const despesa1 = despesaFabrica.adicionarProduto('05/09/2022', 'Comida', 300)
 const despesa2 = despesaFabrica.adicionarProduto('05/09/2022', 'Roupa', 400)
+const despesa3 = despesaFabrica.adicionarProduto('06/09/2022', 'Balada', 1200)
 
-usuario1.adicionarCarteira('vaiTourinho', '05/09/2022')
-usuario1.adicionarCarteira('Itau', '10/09/2022')
-usuario1.adicionarCarteira('Santander', '15/09/2022')
-
-usuario1.alterarNomeCarteira(0, 'vaiUrsinhoooo')
 usuario1.carteiras[0].adicionarProduto(receita1)
 usuario1.carteiras[0].adicionarProduto(receita2)
 usuario1.carteiras[0].adicionarProduto(despesa1)
 usuario1.carteiras[0].adicionarProduto(despesa2)
+usuario1.carteiras[0].adicionarProduto(despesa3)
 
+usuario1.carteiras[0].listarProdutos()
